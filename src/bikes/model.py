9b5f364c34a71854b9cd46c780570bbfa06f4705
Luna Allani <lallani@student.ie.edu>
2021-06-18 17:03:25 +0200

    Pushing Individual Assignment

--- src/bikes/model.py
+++ src/bikes/model.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import numpy as np
 import os.path
-from sklearn.compose import (
-    ColumnTransformer,
-    make_column_transformer,
-    make_column_selector,
-)
+from sklearn.compose import ColumnTransformer, make_column_transformer, make_column_selector
 from sklearn.impute import SimpleImputer, KNNImputer
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer
@@ -18,14 +14,16 @@ __file__ = "__hour.csv__"
 DIRECTORY_WHERE_THIS_FILE_IS = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(DIRECTORY_WHERE_THIS_FILE_IS, "src/bikes/hour.csv")
 
-
 def ffill_missing(ser):
     return ser.fillna(method="ffill")
 
-
 def is_weekend(data):
-    return data["dteday"].dt.day_name().isin(["Saturday", "Sunday"]).to_frame()
-
+    return (
+        data["dteday"]
+        .dt.day_name()
+        .isin(["Saturday", "Sunday"])
+        .to_frame()
+    )
 
 def year(data):
     # Our reference year is 2011, the beginning of the training dataset
@@ -37,6 +35,7 @@ def train_and_persist():
     X = df.drop(columns=["instant", "cnt", "casual", "registered"])
     y = df["cnt"]
 
+
     ffiller = FunctionTransformer(ffill_missing)
 
     weather_enc = make_pipeline(
@@ -46,35 +45,32 @@ def train_and_persist():
         ),
     )
 
+
     ct = make_column_transformer(
         (ffiller, make_column_selector(dtype_include=np.number)),
         (weather_enc, ["weathersit"]),
     )
 
-    preprocessing = FeatureUnion(
-        [
-            ("is_weekend", FunctionTransformer(is_weekend)),
-            ("year", FunctionTransformer(year)),
-            ("column_transform", ct),
-        ]
-    )
 
-    reg = Pipeline(
-        [("preprocessing", preprocessing), ("model", RandomForestRegressor())]
-    )
+    preprocessing = FeatureUnion([
+        ("is_weekend", FunctionTransformer(is_weekend)),
+        ("year", FunctionTransformer(year)),
+        ("column_transform", ct)
+    ])
+
+    reg = Pipeline([("preprocessing", preprocessing), ("model", RandomForestRegressor())])
 
     X_train, y_train = X.loc[X["dteday"] < "2012-10"], y.loc[X["dteday"] < "2012-10"]
     X_test, y_test = X.loc["2012-10" <= X["dteday"]], y.loc["2012-10" <= X["dteday"]]
 
     reg.fit(X_train, y_train)
-    dump(reg, "model.joblib")
-    return "done"
+    dump(reg,"model.joblib")
+    return ("done")
 
-
-def predict(dteday, hr, weathersit, temp, atemp, hum, windspeed):
+def predict(dteday,hr,weathersit,temp,atemp,hum,windspeed):
     reg = load("model.joblib")
 
-    model_data = {
+    model_data= {
         "dteday": [dteday],
         "hr": [hr],
         "weathersit": [weathersit],
@@ -83,14 +79,22 @@ def predict(dteday, hr, weathersit, temp, atemp, hum, windspeed):
         "hum": [hum],
         "windspeed": [windspeed],
     }
-
-    df = pd.DataFrame(
+    
+    df= pd.DataFrame(
         model_data,
-        columns=["dteday", "hr", "weathersit", "temp", "atemp", "hum", "windspeed"],
+        columns=[
+        'dteday',
+        'hr',
+        'weathersit',
+        'temp',
+        'atemp',
+        'hum',
+        'windspeed'
+        ],
     )
 
-    # converting dteday into a datetime
-    df["dteday"] = pd.to_datetime(df["dteday"])
-    y_pred = reg.predict(df)
+#converting dteday into a datetime
+    df["dteday"]= pd.to_datetime(df["dteday"])
+    y_pred= reg.predict(df)
 
     return int(y_pred[0])
